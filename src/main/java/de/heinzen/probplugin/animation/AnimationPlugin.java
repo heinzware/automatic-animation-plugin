0796f10983428ccb9b60b8eb48aa29a2deadb11a
heinzware <ch_1992@hotmail.de>
2017-12-28 14:50:44 +0100

    rename ProBPluginUIConnection to ProBPluginHelper

--- src/main/java/de/heinzen/probplugin/animation/AnimationPlugin.java
+++ src/main/java/de/heinzen/probplugin/animation/AnimationPlugin.java
@@ -1,16 +1,14 @@
 package de.heinzen.probplugin.animation;
 
-import de.prob2.ui.plugin.MenuEnum;
-import de.prob2.ui.plugin.ProBPlugin;
+import de.prob2.ui.plugin.*;
 import de.prob2.ui.prob2fx.CurrentProject;
 import de.prob2.ui.project.Project;
+import de.prob2.ui.project.preferences.Preference;
 import javafx.beans.value.ChangeListener;
 import javafx.scene.control.CheckMenuItem;
 import ro.fortsoft.pf4j.PluginWrapper;
 
 /**
- * Description of class
- *
  * @author Christoph Heinzen
  * @version 0.1.0
  * @since 10.10.17
@@ -21,8 +19,8 @@ public class AnimationPlugin extends ProBPlugin {
     private CurrentProject currentProject;
     private ChangeListener<Project> projectListener;
 
-    public AnimationPlugin(PluginWrapper pluginWrapper) {
-        super(pluginWrapper);
+    public AnimationPlugin(PluginWrapper wrapper, ProBPluginManager manager, ProBPluginHelper helper) {
+        super(wrapper, manager, helper);
     }
 
     @Override
@@ -36,8 +34,8 @@ public class AnimationPlugin extends ProBPlugin {
         currentProject = getInjector().getInstance(CurrentProject.class);
 
         projectListener = (observable, oldValue, newValue) -> {
-            if (newValue != null && currentProject.getRunconfigurations().size() == 1 && enabled()) {
-                currentProject.startAnimation(currentProject.getRunconfigurations().get(0));
+            if (newValue != null && currentProject.getMachines().size() == 1 && menuItem.isSelected()) {
+                currentProject.startAnimation(currentProject.getMachines().get(0), Preference.DEFAULT);
             }
         };
         currentProject.addListener(projectListener);
@@ -45,16 +43,14 @@ public class AnimationPlugin extends ProBPlugin {
 
     @Override
     public void stopPlugin() {
-        getProBConnection().removeMenuItem(MenuEnum.FILE_MENU, menuItem);
+        getProBPluginHelper().removeMenuItem(MenuEnum.FILE_MENU, menuItem);
         currentProject.removeListener(projectListener);
+
     }
 
     private void createMenu() {
         menuItem = new CheckMenuItem("Automatic Animation");
-        getProBConnection().addMenuItem(MenuEnum.FILE_MENU, 3, menuItem);
+        getProBPluginHelper().addMenuItem(MenuEnum.FILE_MENU, 3, menuItem);
     }
 
-    private boolean enabled() {
-        return menuItem.isSelected();
-    }
 }
